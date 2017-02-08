04533bc9689b842aab27997ca7f358067f3e1b93
Vitor Delfino <vitor.delfino952@gmail.com>
2017-02-08 00:24:30 -0200

    tratamento de data de nascimento

--- src/br/com/sppd/resource/SppdResource.java
+++ src/br/com/sppd/resource/SppdResource.java
@@ -20,16 +20,18 @@ import br.com.sppd.retorno.Retorno;
 /**
  * 
  * Classe resposável por conter os metodos REST de acesso ao webservice
+ * 
  * @author Vitor Silva Delfino <vitor.delfino952@gmail.com>
  * @since 2 de dez de 2016
  */
 
 @Path("/sppd")
 public class SppdResource {
-	
+
 	/**
 	 * 
 	 * Método resposável por fazer a chamada ao Controller
+	 * 
 	 * @author Vitor Silva Delfino <vitor.delfino952@gmail.com>
 	 * @since 2 de dez de 2016
 	 * @return List<Estacao>
@@ -37,41 +39,25 @@ public class SppdResource {
 	@GET
 	@Path("/getListaEstacao")
 	@Produces("application/json")
-	public List<Estacao> getListaEstacao(){
+	public List<Estacao> getListaEstacao() {
 		return new EstacaoController().getListaEstacao();
 	}
-	
+
 	@POST
 	@Path("/cadastraPassageiro/{nome}/{cpf}/{rg}/{logradouro}/{numero}/"
 			+ "{complemento}/{cep}/{bairro}/{municipio}/{nascimento}/{deficiente}")
-	@Consumes({"application/json"})
+	@Consumes({ "application/json" })
 	@Produces("application/json")
-	public List<Retorno> cadastraPassageiro(
-			@PathParam("nome") String nome, 
-			@PathParam("cpf") String cpf, 
-			@PathParam("rg") String rg, 
-			@PathParam("logradouro") String logradouro,  
-			@PathParam("numero") String numero,  
-			@PathParam("complemento") String complemento,  
-			@PathParam("cep") String cep,  
-			@PathParam("bairro") String bairro,  
-			@PathParam("municipio") String municipio,  
-			@PathParam("nascimento") String nascimento,
-			@PathParam("deficiente") String deficiente){
-		
-		Passageiro p = new Passageiro(1, 
-				nome, 
-				cpf, 
-				rg, 
-				logradouro, 
-				numero,
-				complemento,
-				cep, 
-				bairro, 
-				municipio, 
-				nascimento, 
-				Boolean.parseBoolean(deficiente));
-		
+	public List<Retorno> cadastraPassageiro(@PathParam("nome") String nome, @PathParam("cpf") String cpf,
+			@PathParam("rg") String rg, @PathParam("logradouro") String logradouro, @PathParam("numero") String numero,
+			@PathParam("complemento") String complemento, @PathParam("cep") String cep,
+			@PathParam("bairro") String bairro, @PathParam("municipio") String municipio,
+			@PathParam("nascimento") String nascimento, @PathParam("deficiente") String deficiente) {
+		nascimento = nascimento.substring(6, nascimento.length()) + "-" + nascimento.substring(3, 5) + "-"
+				+ nascimento.substring(0, 2);
+		Passageiro p = new Passageiro(1, nome, cpf, rg, logradouro, numero, complemento, cep, bairro, municipio,
+				nascimento, Boolean.parseBoolean(deficiente));
+
 		System.out.println(p.toString());
 		List<Retorno> list = new PassageiroController().cadastraPassageiro(p);
 		System.out.println(list.toString());
@@ -81,8 +67,7 @@ public class SppdResource {
 	@GET
 	@Path("/logar/{usuario}/{senha}")
 	@Produces("application/json")
-	public List<LoginBean> logar(@PathParam("usuario") String usuario, 
-								 @PathParam("senha") String senha){
+	public List<LoginBean> logar(@PathParam("usuario") String usuario, @PathParam("senha") String senha) {
 		return new LoginController().logar(usuario, senha);
 	}
 }
