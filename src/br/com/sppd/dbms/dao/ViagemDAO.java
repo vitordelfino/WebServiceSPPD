f3c2af8f8172fda601acd5066d45cc301de6b642
vitor.delfino952 <vitor.delfino952@gmail.com>
2017-03-23 14:23:44 -0400

    metodo de testes para viagem completa

--- src/br/com/sppd/dbms/dao/ViagemDAO.java
+++ src/br/com/sppd/dbms/dao/ViagemDAO.java
@@ -10,7 +10,7 @@ import br.com.sppd.retorno.Retorno;
 
 /**
  * 
- * Classe responsável por realizar métodos ao BD 
+ * Classe responsï¿½vel por realizar mï¿½todos ao BD 
  * @author Vitor Silva Delfino <vitor.delfino952@gmail.com>
  * @since 22 de mar de 2017
  *
@@ -62,6 +62,49 @@ public class ViagemDAO {
 			}
 			
 		}
+	}
+	
+	public void setViagemCompleta(Viagem viagem){
+		Connection = null;
+		PreparedStatement pst = null;
+		String query = "insert into historicoViagem"+
+									 "(codPassageiro, codCartao,dataEntrada, dataSaida, origem, destino,"+
+									 "saldoAnterior, saldoAtual, entrandoSaindo, valor)"+
+									 "values"+
+									 "(?,?,NOW(),NOW(),?,?,?,?,0,?)";
+		Retorno retorno = new Retorno(false,"");
+		
+		try{
+			
+			pst = c.prepareStatement(query);
+			pst.setInt(1, viagem.getPassageiro().getCodPassageiro());
+			pst.setInt(2, viagem.getCartao().getCodCartao());
+			pst.setInt(3, viagem.getOrigem().getCodEstacao());
+			pst.setInt(4, viagem.getDestino().getCodEstacao());
+			pst.setDouble(5, viagem.getSaldoAnterior());
+			pst.setDouble(6, viagem.getSaldoAtual());
+			pst.setDouble(7, viagem.getValor());
+			pst.execute();
+			
+			retorno = new Retorno(true, "Sucess");
+			
+		}catch(Exception sql){
+			System.out.println("********** ERRO DE CONEXAO **********");
+			sql.printStackTrace();
+			retorno.setStatus(sql.getMessage());
+			return retorno;
+		}finally{
+			try {
+				pst.close();
+				c.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
 	}
 
 }
